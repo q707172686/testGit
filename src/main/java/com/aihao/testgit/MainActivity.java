36aae76182368b80c52bde5a2db6f90ac8242fdf
yoyo <jiajia6662008@gmail.com>
2020-06-26 18:05:11 +0900

    test

--- src/main/java/com/aihao/testgit/MainActivity.java
+++ src/main/java/com/aihao/testgit/MainActivity.java
@@ -3,12 +3,15 @@ package com.aihao.testgit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // test
+        Log.i("test","test");
         setContentView(R.layout.activity_main);
     }
 }
